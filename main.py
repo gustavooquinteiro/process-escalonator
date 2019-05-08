3902907148f6e0ccb552495a24f74ec5737bdee3
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-05-08 10:10:54 -0300

    Calculando o tempo de turnaround médio da execução dos processos

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ from ioqueue import IO
 from cpu import CPU
 import random
 
-quantum = int(input("Quantum: "))
-override = int(input("Sobrecarga: "))
-escalonator = Escalonator("RR", override)
-c = CPU(escalonator, quantum)
-escalonator.cpu = c
-io = IO()
-io.escalonator = escalonator
-processes = []
+def main():
+    quantum = int(input("Quantum: "))
+    override = int(input("Sobrecarga: "))
+    type = str(input("Tipo de algoritmo de escalonamento (FCFS | SJF | RR | EDF): "))
+    escalonator = Escalonator(type.upper(), override)
+    cpu = CPU(escalonator, quantum)
+    escalonator.cpu = cpu
+    #io = IO()
+    #io.escalonator = escalonator
+    n = int(input("Quantidade de processos: "))
+    for i in range(n):
+        id = i 
+        print("Criando o processo {}: " .format(id))
+        start = int(input("Tempo de chegada: "))
+        execution_time = int(input("Tempo de execução: "))
+        deadline = int(input("Deadline: "))    
+        p = Process(id, start, execution_time, deadline)
+        escalonator.ready_queue.append(p)
+        
+    escalonator.queue()
+    cpu.run()
+    
+    turnaround = sum(cpu.concluded_process_time)/n
+    print("Turnaround == {}" .format(turnaround))
 
-n = int(input("Quantidade de processos: "))
-for i in range(n):
-    id = random.randint(1000, 3000)
-    start = int(input("Tempo de chegada: "))
-    execution_time = int(input("Tempo de execução: "))
-    deadline = int(input("Deadline: "))    
-    
-    print("Criando o processo:\n\tID: {}\n\tTempo de inicio: {}\n\tTempo de execução: {}\n\tDeadline: {}\n" .format(id, start, execution_time, deadline                                                                                                                                                                                                                                                  ))
-    p = Process(id, start, execution_time, deadline, io)
-    escalonator.ready_queue.append(p)
-    
-escalonator.queue()
-    
+if __name__ == "__main__":
+    main()
